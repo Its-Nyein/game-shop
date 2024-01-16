baef42e697546b754b5aa87e0d8ba958147ff922
Nyein Phyo Aung <nyeinphyoaung.edu@gmail.com>
2024-01-16 18:43:16 +0630

    carousel slider with effectCoverflow

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./main.css";
 import SlideMenu from "../components/SlideMenu";
 import Header from "./Header";
+import Home from "./Home";
 
 function Main() {
   const [active, setActive] = useState(false);
+  const [games, setGames] = useState([]);
 
   const handleToggleActive = () => {
     setActive(!active);
   };
+
+  const fetchData = () => {
+    fetch("http://localhost:3000/api/gamesData.json")
+      .then((res) => res.json())
+      .then((data) => setGames(data))
+      .catch((e) => console.log(e.message));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <main>
       <SlideMenu active={active} />
       <div className={`banner ${active ? "active" : undefined}`}>
         <Header toggleHandle={handleToggleActive} />
+        <div className="container-fluid">
+          <Home games={games} />
+        </div>
       </div>
     </main>
   );
